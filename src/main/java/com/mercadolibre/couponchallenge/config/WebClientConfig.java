dd6fc0fae111e9d6bbf66f2059b1f080c4c2d7d3
Emanuel Guillermo Vallejo Cadena <egvallejo1@gmail.com>
2024-07-01 17:56:58 -0500

    se guardan cambios

--- src/main/java/com/mercadolibre/couponchallenge/config/WebClientConfig.java
+++ src/main/java/com/mercadolibre/couponchallenge/config/WebClientConfig.java
@@ -2,12 +2,16 @@ package com.mercadolibre.couponchallenge.config;
 
 import com.mercadolibre.couponchallenge.clients.MercadoLibreItemsClient;
 import com.mercadolibre.couponchallenge.clients.MercadoLibreReviewsClient;
+import io.netty.channel.ChannelOption;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -15,7 +19,12 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(@Value("${app.api.url}") String urlBase) {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000); // 1-second timeout
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
         return WebClient.builder()
+                .clientConnector(connector)
                 .baseUrl(urlBase)
                 .build();
     }
--- src/main/java/com/mercadolibre/couponchallenge/dto/api/items/request/ItemsCouponRequest.java
+++ src/main/java/com/mercadolibre/couponchallenge/dto/api/items/request/ItemsCouponRequest.java
@@ -1,13 +1,16 @@
 package com.mercadolibre.couponchallenge.dto.api.items.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record ItemsCouponRequest(
+        @NotEmpty(message = "items vacios")
         @JsonProperty("items_ids")
         List<String> itemsIds,
-        //@Min(message = "cannot be less than 1", value = 1)
+        @Min(message = "cannot be less than 1", value = 1)
         Integer amount
 ) {
 
